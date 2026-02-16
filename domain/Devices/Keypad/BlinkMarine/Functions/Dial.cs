62f56ea18338e9512cf565d9d20e891adb0becb1
Cory Grant <cory.grant11@gmail.com>
2026-02-15 22:58:00 -0500

    add pdm grayhill and dial

--- domain/Devices/Keypad/BlinkMarine/Functions/Dial.cs
+++ domain/Devices/Keypad/BlinkMarine/Functions/Dial.cs
@@ -8,7 +8,11 @@ public class Dial(int number, string name)
 {
     [JsonPropertyName("number")] public int Number { get; } = number;
     [JsonPropertyName("name")] public string Name { get; set; } = name;
+    [JsonPropertyName("enabled")] public bool Enabled { get; set; }
     [JsonPropertyName("topPosition")] public int TopPosition { get; set; }
+    [JsonPropertyName("minLed")] public int MinLed { get; set; }
+    [JsonPropertyName("maxLed")] public int MaxLed { get; set; }
+    [JsonPropertyName("ledOffset")] public int LedOffset { get; set; }
     [JsonIgnore] public int Position { get; set; }  // Ticks
     [JsonIgnore] public int Delta { get; set; }   
     [JsonIgnore] public DialDirection Direction { get; set; }
